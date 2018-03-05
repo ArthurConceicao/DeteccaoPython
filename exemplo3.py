fa14f1ad6d6a7fb0159d2b0ece64a704b0bd01da
ArthurConceicao <33375414+ArthurConceicao@users.noreply.github.com>
2018-03-05 17:45:09 -0300

    Testando commit com push

--- exemplo3.py
+++ exemplo3.py
@@ -1,9 +1,15 @@
 import cv2
 
-video = cv2.VideoCapture('videos\\video.avi')
+video = cv2.VideoCapture('videos\\video2.mp4')
+classificador_face = cv2.CascadeClassifier('cascades\\haarcascade_frontalface_default.xml')
 
 while True:
     conectado, frame = video.read()
+
+    frame_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces_detectadas = classificador_face.detectMultiScale(frame_cinza)
+    for (x, y, l, a) in faces_detectadas:
+        cv2.rectangle(frame, (x, y), (x + l, y + a), (0, 0, 255), 2)
 
     cv2.imshow('Video', frame)
 
