845f8934c13060c4dde518837726ddf1e0414196
ArthurConceicao <33375414+ArthurConceicao@users.noreply.github.com>
2018-03-07 23:40:48 -0300

    exemplo4

--- exemplo3.py
+++ exemplo3.py
@@ -3,6 +3,7 @@ import cv2
 #video = cv2.VideoCapture('videos\\video.mp4')
 video = cv2.VideoCapture(0)
 classificador_face = cv2.CascadeClassifier('cascades\\haarcascade_frontalface_default.xml')
+classificador_olhos = cv2.CascadeClassifier('cascades\\haarcascade_eye.xml')
 
 while True:
     conectado, frame = video.read()
@@ -10,7 +11,14 @@ while True:
     frame_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces_detectadas = classificador_face.detectMultiScale(frame_cinza)
     for (x, y, l, a) in faces_detectadas:
-        cv2.rectangle(frame, (x, y), (x + l, y + a), (0, 0, 255), 2)
+        # imagem = cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 2)
+        regiao = frame[y:y + a, x:x + l]
+        regiao_cinza_olho = cv2.cvtColor(regiao, cv2.COLOR_BGR2GRAY)
+        olhos_detectados = classificador_olhos.detectMultiScale(regiao_cinza_olho, scaleFactor=1.1, minNeighbors=10)
+        print(olhos_detectados)
+        for (x2, y2, l2, a2) in olhos_detectados:
+            frame = cv2.rectangle(frame, (x, y), (x + l, y + a), (0, 0, 255), 2)
+            regiao = cv2.rectangle(regiao, (x2, y2), (x2 + l2, y2 + a2), (255, 0, 0), 2)
 
     cv2.imshow('Video', frame)
 
